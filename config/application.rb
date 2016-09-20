24eef964e8de0ece922d85bccbc81eae6308b375
Elton Murillo <emurillo510@gmail.com>
2016-09-19 19:15:19 -0700

    updates application_controller

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module RailsBart
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # CORS Settings
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
